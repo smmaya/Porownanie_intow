b8f979999d5419107c3595972860b8ba7a098c2e
Sławomir Majchrzak <52676366+smmaya@users.noreply.github.com>
2021-06-29 12:54:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Nauka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 12;
+            int num2 = 11;
+
+            if (num1 < num2)
+            {
+                Console.WriteLine(num1 + " jest mniejsze od " + num2);
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine(num1 + " jest równe " + num2);
+            }
+            else
+            {
+                Console.WriteLine(num1 + " jest większe od " + num2);
+            }
+        }
+    }
+}
